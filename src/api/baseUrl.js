589395f57f9ffe8eb8a1d872460ca4af90ec0182
unknown <mamiki85@gmail.com>
2020-08-30 14:46:34 +0100

    production buil

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,11 +1,11 @@
-export default function getBaseurl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http//localhost:3001/' : '/';
+export default function getBaseUrl() {
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*) |&|#|$"),
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
       results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
